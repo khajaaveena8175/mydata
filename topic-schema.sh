bd990cea3191010907ded5647771cabac62f45a8
khajaaveena8175 <55309404+khajaaveena8175@users.noreply.github.com>
2024-06-27 09:29:11 +0530

    Update topic-schema.sh

--- topic-schema.sh
+++ topic-schema.sh
@@ -9,34 +9,26 @@ echo ${topic_list[@]}
 
 for topic  in "${topic_list[@]}"
 do
-
+  echo $topic
 subject=$topic-value
-curl "${curlCMD[@]}"/${subject}/versions/latest/schema | jq '.'  >  /home/gopalkrishna/dev/${subject}.avsc
+curl "${curlCMD[@]}"/${subject}/versions/latest/schema | jq '.'  > ${GIT_REPO}/${ENV}/${subject}.avsc
 
 done
-
-
-
-
-echo "****************************************************************"
-#https://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@github.com/khaja.aveena@gmail.com/mydata.git
-
-
-#https://github.com/khajaaveena8175/mydata.git
 
 
 
 cd ${GIT_REPO}
 
 
-#git add --all 
+git add --all 
 
-#DATE=$(date)
+DATE=$(date)
 
 
-#git commit -q -m "changes made on $DATE"
-#
-#git push -q origin master
+git commit -q -m "changes made on $DATE"
 
-#git commit -q https://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@github.com/khajaaveena8175/mydata.git
-#git push -q https://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@github.com/khajaaveena8175/mydata.git
+git push -q origin master
+
+
+
+echo "****************************************************************"

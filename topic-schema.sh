14e8fb188f802cf6581650be41284fe265c73e5c
khajaaveena8175 <55309404+khajaaveena8175@users.noreply.github.com>
2024-06-27 09:08:40 +0530

    Update topic-schema.sh

--- topic-schema.sh
+++ topic-schema.sh
@@ -1,56 +1,24 @@
 #!/bin/sh
 
-
-IFS=',' 
-
-topic_list=$1
-
-
+IFS=','
 
 curlCMD=(-s -X GET "$SCHEMA_REGISTRY" )
+read -a topic_list <<<"$(cat topic-list.csv)"
 
+echo ${topic_list[@]}
 
-
-read -a subjects <<<"$(curl "${curlCMD[@]}"| sed 's/[][]//g')"
-
-
-#read -a subjects <<<"$(eval "$schema_registry" | sed 's/[][]//g')"
-#read -a subjects <<<"$($schema_registry | sed 's/[][]//g')"
-
-
-for subject  in "${subjects[@]}"
+for topic  in "${topic_list[@]}"
 do
-  echo $subject
 
-
-
-
-
-if [[ $subject == *"gdr."* ||  $subject == *"dap."* ]]; then
-  subject=${subject//\"/}
-
-#curl -s  -X GET http://localhost:8081/subjects/${subject}/versions/latest  | tr -d '\\'> /home/gopalkrishna/gitfolder/mydata/${ENV}/mydata-value-46.avsc
-
-
-#eval "$schema_registry/${subject}/versions/latest > /home/gopalkrishna/gitfolder/mydata/${ENV}/${subject}.avsc"
-
-
-
+subject=$topic-value
 curl "${curlCMD[@]}"/${subject}/versions/latest/schema | jq '.'  >  /home/gopalkrishna/dev/${subject}.avsc
-
-#schema=$(curl --silent  -X GET http://localhost:8081/subjects/${subject}/versions/latest) 
-#${schema//\\/} >  mynew-value.avsc
-#output1=${schema//\\/} 
-
-#echo "$output1" > mynew-value.avsc
-
-fi
-
-
 
 done
 
 
+
+
+echo "****************************************************************"
 #https://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@github.com/khaja.aveena@gmail.com/mydata.git
 
 
